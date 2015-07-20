1442dfe2ade8b73bb0d8aeac5acc647d2ecf7806
Vinod <j.vinod@portworx.com>
2015-07-20 10:12:44 +0000

    move kvdb out of pkg into its own repo

---
+++ etcd/kv_etcd_test.go
@@ -0,0 +1,22 @@
+package etcd
+
+import (
+	"testing"
+
+	"github.com/portworx/kvdb"
+	"github.com/portworx/kvdb/test"
+)
+
+func TestAll(t *testing.T) {
+	kv, err := kvdb.New(KVEtcd, "pwx/test", nil, nil)
+	if err != nil {
+		t.Fatalf("Failed to initialize KVDB: %v", err)
+	}
+
+	err = kvdb.SetInstance(kv)
+	if err != nil {
+		t.Fatalf("Failed to set instance: %v", err)
+	}
+
+	test.Run(t)
+}
