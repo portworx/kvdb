2da8abefda693493ab13812217b4899ad07cac71
ganesh <ganesh@portworx.com>
2016-11-14 13:51:00 -0500

    Do not invoke the callback if kvdb key pair is null.

--- updates_collector.go
+++ updates_collector.go
@@ -62,7 +62,7 @@ func (c *updatesCollectorImpl) ReplayUpdates(cbList []ReplayCb) (uint64, error) 
 			index = update.kvp.KVDBIndex
 		}
 		for _, cbInfo := range cbList {
-			if update.kvp == nil ||
+			if update.kvp != nil &&
 				strings.HasPrefix(update.kvp.Key, cbInfo.Prefix) {
 				err := cbInfo.WatchCB(update.prefix, cbInfo.Opaque, update.kvp,
 					update.err)
