e6706661a28004698b24ef7451985675dcea2ef1
Aditya Dani <aditya@portworx.com>
2017-06-03 18:50:57 +0100

    Add a unit test for adding a removed member

--- test/kv_controller.go
+++ test/kv_controller.go
@@ -46,15 +46,18 @@ func RunControllerTests(datastoreInit kvdb.DatastoreInit, t *testing.T) {
 
 	testAddMember(kv, t)
 	testRemoveMember(kv, t)
+	testReAdd(kv, t)
+	controllerLog("Stopping all etcd processes")
 	for _, cmd := range cmds {
 		cmd.Process.Kill()
 	}
 }
 
 func testAddMember(kv kvdb.Kvdb, t *testing.T) {
-	fmt.Println("testAddMember")
+	controllerLog("testAddMember")
 	// Add node 1
 	index := 1
+	controllerLog("Adding node 1")
 	initCluster, err := kv.AddMember(localhost, peerPorts[index], names[index])
 	require.NoError(t, err, "Error on AddMember")
 	require.Equal(t, 2, len(initCluster), "Init Cluster length does not match")
@@ -69,18 +72,25 @@ func testAddMember(kv kvdb.Kvdb, t *testing.T) {
 }
 
 func testRemoveMember(kv kvdb.Kvdb, t *testing.T) {
-	fmt.Println("testRemoveMember")
+	controllerLog("testRemoveMember")
 	// Add node 2
 	index := 2
+	controllerLog("Adding node 2")
 	initCluster, err := kv.AddMember(localhost, peerPorts[index], names[index])
 	require.NoError(t, err, "Error on AddMember")
 	require.Equal(t, 3, len(initCluster), "Init Cluster length does not match")
 	cmd, err := startEtcd(index, initCluster, "existing")
 	require.NoError(t, err, "Error on start etcd")
 	cmds[index] = cmd
+	// Check the list returned
+	list, err := kv.ListMembers()
+	require.NoError(t, err, "Error on ListMembers")
+	require.Equal(t, 3, len(list), "List returned different length of cluster")
+
 	
 	// Remove node 1
 	index = 1
+	controllerLog("Removing node 1")
 	err = kv.RemoveMember(names[index])
 	require.NoError(t, err, "Error on RemoveMember")
 
@@ -88,9 +98,30 @@ func testRemoveMember(kv kvdb.Kvdb, t *testing.T) {
 	cmd.Process.Kill()
 	delete(cmds, index)
 	// Check the list returned
-	list, err := kv.ListMembers()
+	list, err = kv.ListMembers()
 	require.NoError(t, err, "Error on ListMembers")
 	require.Equal(t, 2, len(list), "List returned different length of cluster")
+}
+
+func testReAdd(kv kvdb.Kvdb, t *testing.T) {
+	controllerLog("testReAdd")
+	// Add node 1 back
+	index := 1
+	controllerLog("Re-adding node 1")
+	// For re-adding we need to delete the data-dir of this member
+	os.RemoveAll(dataDirs[index])
+	initCluster, err := kv.AddMember(localhost, peerPorts[index], names[index])
+	require.NoError(t, err, "Error on AddMember")
+	require.Equal(t, 3, len(initCluster), "Init Cluster length does not match")
+	cmd, err := startEtcd(index, initCluster, "existing")
+	require.NoError(t, err, "Error on start etcd")
+	cmds[index] = cmd
+
+	// Check the list returned
+	list, err := kv.ListMembers()
+	require.NoError(t, err, "Error on ListMembers")
+	require.Equal(t, 3, len(list), "List returned different length of cluster")
+
 }
 
 func startEtcd(index int, initCluster map[string][]string, initState string) (*exec.Cmd, error){
@@ -138,4 +169,10 @@ func cleanup() {
 		os.RemoveAll(dir)
 		os.MkdirAll(dir, 0777)
 	}
+}
+
+func controllerLog(log string) {
+	fmt.Println("--------------------")
+	fmt.Println(log)
+	fmt.Println("--------------------")
 }
