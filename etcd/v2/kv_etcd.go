d8a8a52dedd8087ed296be150eca53009d22557f
Aditya Dani <aditya@portworx.com>
2018-01-13 02:33:19 +0000

    Fix a race condition in Snapshot implementation which caused a go panic.
    - For the following scenario the Snapshot API would panic for any kvdb implementation in snapMem.Delete function
    
    Following is the timeline of calls that Snapshot API does
    Time    API
    t1 --> Watch
    .
    .
    t2 --> Enumerate
    .
    t3 --> SnapPut
    .
    .
    t4 --> Apply updates from Watch to snapDB
    
    If there are any Delete calls to the actual kvdb between t1 and t2 those
    keys will not be present in the Snapshot in-mem db. However the Watch will
    record the Delete request. At t4 while applying the updates we will try to
    Delete a non-existent key from snapMem and panic.

--- etcd/v2/kv_etcd.go
+++ etcd/v2/kv_etcd.go
@@ -762,6 +762,11 @@ func (kv *etcdKV) Snapshot(prefix string) (kvdb.Kvdb, uint64, error) {
 			kvPair.ModifiedIndex > lowestKvdbIndex {
 			if kvPair.Action == kvdb.KVDelete {
 				_, err = snapDb.Delete(kvPair.Key)
+				// A Delete key was issued between our Watch and Enumerate
+				// APIs in this function
+				if err == kvdb.ErrNotFound {
+					err = nil
+				}
 			} else {
 				_, err = snapDb.SnapPut(kvPair)
 			}
